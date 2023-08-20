6debf835e98d5e21eb1431dbc5d75e027ecf2a52
systemcomputer.bangalore@gmail.com <systemcomputer.bangalore@gmail.com>
2023-08-20 05:21:13 +0000

    Created designation, department and employee; Edited emp_add

---
+++ server_code/employee.py
@@ -0,0 +1,33 @@
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+
+
+######### auto increment 'id' column ###########
+@anvil.server.callable
+def emp_get_next_id_value():
+  # Get the last row of the data table
+  next_value = '0000000001'
+  try:
+    trans_code_list = [(r["id"]) for r in app_tables.employee.search()]
+    last_row = trans_code_list[-1]
+    last_string_value = last_row
+    next_value = str(int(last_string_value) + 1).zfill(10)
+  except IndexError:
+    next_value == '0000000001'
+  return next_value
