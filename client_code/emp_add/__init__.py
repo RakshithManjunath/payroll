6b3d7a270694f7dbfae8e92e7b8dd269fd1b53cd
systemcomputer.bangalore@gmail.com <systemcomputer.bangalore@gmail.com>
2023-08-20 12:34:18 +0000

    Edited emp_add

--- client_code/emp_add/__init__.py
+++ client_code/emp_add/__init__.py
@@ -16,13 +16,19 @@ class emp_add(emp_addTemplate):
   def link_1_click(self, **event_args):
     """This method is called when the link is clicked"""
     self.custom_1.visible = not self.custom_1.visible
+    self.custom_2.visible = False
+    self.custom_3.visible = False
 
   def link_2_click(self, **event_args):
     """This method is called when the link is clicked"""
+    self.custom_1.visible = False
     self.custom_2.visible = not self.custom_2.visible
+    self.custom_3.visible = False
 
   def link_3_click(self, **event_args):
     """This method is called when the link is clicked"""
+    self.custom_1.visible = False
+    self.custom_2.visible = False
     self.custom_3.visible = not self.custom_3.visible
 
   def button_2_click(self, **event_args):
