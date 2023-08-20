d9a7d3cfec3f891314f3d6e37de7577d23167843
systemcomputer.bangalore@gmail.com <systemcomputer.bangalore@gmail.com>
2023-08-20 06:17:05 +0000

    Created test_mod and test; Edited settings and emp_add

--- client_code/test/__init__.py
+++ client_code/test/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import emp_addTemplate
+from ._anvil_designer import testTemplate
 from anvil import *
 import anvil.server
 import anvil.users
@@ -6,16 +6,12 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class emp_add(emp_addTemplate):
+class test(testTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def call_dept_names(self, **kwargs):
-    data = anvil.server.call('get_dept_names')
-    self.dept_dp.items = data
 
   def save_to_database(self, db_val, **kwargs):
     anvil.server.call('save_data', db_val)
