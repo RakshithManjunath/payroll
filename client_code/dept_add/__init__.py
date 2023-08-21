1b36b24b305e748cede11389e09a6db42ccb83ff
systemcomputer.bangalore@gmail.com <systemcomputer.bangalore@gmail.com>
2023-08-21 08:44:32 +0000

    Edited dept_add

--- client_code/dept_add/__init__.py
+++ client_code/dept_add/__init__.py
@@ -12,3 +12,8 @@ class dept_add(dept_addTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+  anvil.server.call('dept_add',self.value, self.text_box_1.text)
+
