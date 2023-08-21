399dad5a597958960900c98a4463b17d4734de22
systemcomputer.bangalore@gmail.com <systemcomputer.bangalore@gmail.com>
2023-08-21 08:39:05 +0000

    Edited department

--- server_code/department.py
+++ server_code/department.py
@@ -16,6 +16,26 @@ import anvil.server
 #   print("Hello, " + name + "!")
 #   return 42
 #
+############### Add department ############
+@anvil.server.callable
+def dept_get_next_string_value():
+  # Get the last row of the data table
+  next_value = '001'
+  try:
+    dept_code_list = [(r["dept_code"]) for r in app_tables.dept.search()]
+    last_row = dept_code_list[-1]
+    last_string_value = last_row
+    next_value = str(int(last_string_value) + 1).zfill(3)
+  except IndexError:
+    next_value == '001'
+  return next_value
+
+
+
+
+
+
+
 
 @anvil.server.callable
 def get_dept_names():
