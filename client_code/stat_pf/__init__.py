23321bce129d9307b1c792f4a34817f7759460a4
systemcomputer.bangalore@gmail.com <systemcomputer.bangalore@gmail.com>
2023-08-27 07:21:41 +0000

    Created stat_pf and stat_esi_pt; Edited settings and statutary; Deleted stat_esi_pf_pt

--- client_code/stat_pf/__init__.py
+++ client_code/stat_pf/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import stat_esi_pf_ptTemplate
+from ._anvil_designer import stat_pfTemplate
 from anvil import *
 import anvil.server
 import anvil.users
@@ -6,7 +6,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class stat_esi_pf_pt(stat_esi_pf_ptTemplate):
+class stat_pf(stat_pfTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
--- client_code/statutary/__init__.py
+++ client_code/statutary/__init__.py
@@ -12,3 +12,17 @@ class statutary(statutaryTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.custom_1.visible = not self.custom_1.visible
+    self.custom_2.visible = False
+
+  def link_2_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.custom_1.visible = False
+    self.custom_2.visible = not self.custom_2.visible
+
+
+
+
