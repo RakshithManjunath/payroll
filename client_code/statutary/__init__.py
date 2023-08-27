a14cc8016149f0b9c7ac39f9fa1aa5ea17d41d4c
systemcomputer.bangalore@gmail.com <systemcomputer.bangalore@gmail.com>
2023-08-27 08:10:48 +0000

    Created stat_earnings, stat_earnings.RowTemplate3 and stat_esi_pt.RowTemplate2; Edited stat_pf, stat_esi_pt and statutary

--- client_code/statutary/__init__.py
+++ client_code/statutary/__init__.py
@@ -17,11 +17,20 @@ class statutary(statutaryTemplate):
     """This method is called when the link is clicked"""
     self.custom_1.visible = not self.custom_1.visible
     self.custom_2.visible = False
+    self.custom_3.visible = False
 
   def link_2_click(self, **event_args):
     """This method is called when the link is clicked"""
     self.custom_1.visible = False
     self.custom_2.visible = not self.custom_2.visible
+    self.custom_3.visible = False
+
+  def link_3_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.custom_1.visible = False
+    self.custom_2.visible = False
+    self.custom_3.visible = not self.custom_3.visible
+   
 
 
 
