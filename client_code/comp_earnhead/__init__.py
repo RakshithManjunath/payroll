72e21ae7f2f434a9351988c4cf972a899dfb06c2
systemcomputer.bangalore@gmail.com <systemcomputer.bangalore@gmail.com>
2023-08-28 13:25:31 +0000

    Edited company and comp_earnhead

--- client_code/comp_earnhead/__init__.py
+++ client_code/comp_earnhead/__init__.py
@@ -14,9 +14,16 @@ class comp_earnhead(comp_earnheadTemplate):
 
     # Any code you write here will run before the form opens.
     comp_details = anvil.server.call('comp_get_details', gvarb.g_comcode)
-    self.text_box_2.text = comp_details['comp_emp_pfrate']
+    self.text_box_1.text = comp_details['comp_earn_head1']
+    self.text_box_3.text = comp_details['comp_earn_head2']
 
   def button_2_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form('comp_more')
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.server.call('comp_more_update_earnhead', gvarb.g_comcode, self.text_box_1.text,
+                     self.text_box_3.text)
+
 
--- server_code/company.py
+++ server_code/company.py
@@ -104,3 +104,11 @@ def statutary_update_ded(comp_code, comp_ded1,comp_ded2,comp_ded3,comp_ded4):
             comp_ded3=comp_ded3,
             comp_ded4=comp_ded4)
 
+
+####### company more earan head  update ########
+@anvil.server.callable
+def comp_more_update_earnhead(comp_code,comp_earnhead1,comp_earnhead2):
+  row = app_tables.company.get(comp_code=comp_code)
+  row.update(comp_earn_head1=comp_earnhead1)
+  row.update(comp_earn_head2=comp_earnhead2)
+
