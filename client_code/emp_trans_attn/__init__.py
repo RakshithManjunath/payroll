755b00237ac4bb88f5b6a96f14d3c05d12311836
systemcomputer.bangalore@gmail.com <systemcomputer.bangalore@gmail.com>
2023-08-31 13:11:07 +0000

    Edited emp_trans_ded, emp_trans_misc and emp_trans_attn

--- client_code/emp_trans_attn/__init__.py
+++ client_code/emp_trans_attn/__init__.py
@@ -5,6 +5,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from .. import gvarb
 
 class emp_trans_attn(emp_trans_attnTemplate):
   def __init__(self, **properties):
@@ -12,3 +13,10 @@ class emp_trans_attn(emp_trans_attnTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    comp_details = anvil.server.call('comp_get_details', gvarb.g_comcode)
+
+    self.label_6.text = comp_details['comp_leave_head1']
+    self.label_7.text = comp_details['comp_leave_head2']
+    self.label_8.text = comp_details['comp_leave_head3']
+
+
--- client_code/emp_trans_ded/__init__.py
+++ client_code/emp_trans_ded/__init__.py
@@ -5,6 +5,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from .. import gvarb
 
 class emp_trans_ded(emp_trans_dedTemplate):
   def __init__(self, **properties):
@@ -12,3 +13,18 @@ class emp_trans_ded(emp_trans_dedTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    comp_details = anvil.server.call('comp_get_details', gvarb.g_comcode)
+
+    self.label_1.text = comp_details['comp_ded1']
+    self.label_2.text = comp_details['comp_ded2']
+    self.label_3.text = comp_details['comp_ded3']
+    self.label_4.text = comp_details['comp_ded4']
+
+    self.label_5.text = comp_details['comp_loan_head1']
+    self.label_6.text = comp_details['comp_loan_head2']
+    
+
+    
+    
+    
+
