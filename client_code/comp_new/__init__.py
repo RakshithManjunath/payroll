4e5655ccb65302c9431816f08c78c18f8097cc97
systemcomputer.bangalore@gmail.com <systemcomputer.bangalore@gmail.com>
2023-09-11 09:04:31 +0000

    Edited settings and comp_new

--- client_code/comp_new/__init__.py
+++ client_code/comp_new/__init__.py
@@ -12,7 +12,13 @@ class comp_new(comp_newTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    month_names_alphabets = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    month_names_numeric = ["01","02","03","04","05","06","07","08","09","10","11","12"]
+    year_names = ["2023", "2024", "2025", "2026"]
 
+    self.drop_down_1.items = month_names_alphabets
+    self.drop_down_2.items = year_names
+    
   def button_2_click(self, **event_args):
     """This method is called when the button is clicked"""
     anvil.users.login_with_form()
