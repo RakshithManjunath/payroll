b7a91bc01eb9147e71119f2b268084e5ed19a4a2
systemcomputer.bangalore@gmail.com <systemcomputer.bangalore@gmail.com>
2023-09-11 05:13:29 +0000

    Created password; Edited logform

--- client_code/logform/__init__.py
+++ client_code/logform/__init__.py
@@ -12,3 +12,8 @@ class logform(logformTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.server.call('check_username_and_password', self.text_box_1, self.text_box_2)
+
---
+++ server_code/password.py
@@ -0,0 +1,29 @@
+import anvil.files
+from anvil.files import data_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+
+@anvil.server.callable
+def check_username_and_password(username, password):
+  password_table = app_tables.password.search()
+  db_username, db_password = password[username], password_table[password]
+
+@anvil.server.callable
+def check_password_and_confirm_password(username, password):
+  pass
