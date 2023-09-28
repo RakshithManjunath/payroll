d34d9c7b53791ee4efeb52691c106091661b7e8f
systemcomputer.bangalore@gmail.com <systemcomputer.bangalore@gmail.com>
2023-09-28 01:02:05 +0000

    Edited settings and comp_change

--- client_code/comp_change/__init__.py
+++ client_code/comp_change/__init__.py
@@ -5,6 +5,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from .. import gvarb
 
 class comp_change(comp_changeTemplate):
   def __init__(self, **properties):
@@ -12,56 +13,34 @@ class comp_change(comp_changeTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-    self.row = anvil.server.call('comp_get_details',self.comp_code)
-  
-    self.text_box_1.text = self.row['comp_addr1']
-    self.text_box_2.text = self.row['comp_addr2']
-    self.text_box_3.text = self.row['comp_addr3']
-    self.text_box_4.text = self.row['comp_pf_number']
-    self.text_box_5.text = self.row['comp_esi_number']
-    self.text_box_6.text = self.row['comp_pto_circle']
-  
+    comp_details = anvil.server.call('comp_get_details', gvarb.g_comcode)
+    #self.row = anvil.server.call('comp_get_details',gvarb.g_comcode)
+    
+    self.text_box_7.text = comp_details['comp_name']
+    self.text_box_1.text = comp_details['comp_addr1']
+    self.text_box_2.text = comp_details['comp_addr2']
+    self.text_box_3.text = comp_details['comp_addr3']
+    self.text_box_4.text = comp_details['comp_pf_number']
+    self.text_box_5.text = comp_details['comp_esi_number']
+    self.text_box_6.text = comp_details['comp_pto_circle']
   
   #self.drop_down_1.items = anvil.server.call('comp_change_name_and_code')
 
-  def drop_down_1_change(self, **event_args):
-    """This method is called when an item is selected"""
-    # self.text_box_7.visible = True
-    # if self.drop_down_1.selected_value is not None:
-    #   split_list_comp = self.drop_down_1.selected_value.split("|")
-    #   split_list_comp = [ele.strip() for ele in split_list_comp] 
-    #   self.comp_code,self.comp_name = split_list_comp[0],split_list_comp[1]
-  
-      # self.row = anvil.server.call('comp_get_details',self.comp_code)
-  
-      # self.text_box_1.text = self.row['comp_addr1']
-      # self.text_box_2.text = self.row['comp_addr2']
-      # self.text_box_3.text = self.row['comp_addr3']
-      # self.text_box_4.text = self.row['comp_pf_number']
-      # self.text_box_5.text = self.row['comp_esi_number']
-      # self.text_box_6.text = self.row['comp_pto_circle']
-    #   self.text_box_7.text = self.comp_name
-    #   self.drop_down_1.visible = False
-    # else:
-    #   self.button_1.enabled = False
-      # self.text_box_5.text = self.bank_name
+  # def button_1_click(self, **event_args):
+  #   """This method is called when the button is clicked"""
+  #   if self.text_box_7.text == "":
+  #     Notification("Company name cannot be blank").show()
+  #   else:
+  #     anvil.server.call('comp_update',self.comp_code,
+  #     self.text_box_1.text, self.text_box_2.text,
+  #     self.text_box_3.text,self.text_box_4.text,
+  #     self.text_box_5.text,self.text_box_6.text,
 
-  def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    if self.text_box_7.text == "":
-      Notification("Company name cannot be blank").show()
-    else:
-      anvil.server.call('comp_update',self.comp_code,
-      self.text_box_1.text, self.text_box_2.text,
-      self.text_box_3.text,self.text_box_4.text,
-      self.text_box_5.text,self.text_box_6.text,
-    #self.drop_down_1.items = anvil.server.call('comp_change_name_and_code')
+  # # def button_2_click(self, **event_args):
+  # #   """This method is called when the button is clicked"""
+  # #     open_form('comp_change')
 
-  def button_2_click(self, **event_args):
-    """This method is called when the button is clicked"""
-      open_form('comp_change')
-
-  def outlined_button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    open_form('menu')
+  # # def outlined_button_1_click(self, **event_args):
+  # #   """This method is called when the button is clicked"""
+  # #   open_form('menu')
 
