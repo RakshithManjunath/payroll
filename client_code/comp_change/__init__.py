d1907467a358fae7caf2deb816cc49f8b7a2c7c1
systemcomputer.bangalore@gmail.com <systemcomputer.bangalore@gmail.com>
2023-09-28 00:06:40 +0000

    Edited comp_change

--- client_code/comp_change/__init__.py
+++ client_code/comp_change/__init__.py
@@ -12,28 +12,38 @@ class comp_change(comp_changeTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-    self.drop_down_1.items = anvil.server.call('comp_change_name_and_code')
+    self.row = anvil.server.call('comp_get_details',self.comp_code)
+  
+    self.text_box_1.text = self.row['comp_addr1']
+    self.text_box_2.text = self.row['comp_addr2']
+    self.text_box_3.text = self.row['comp_addr3']
+    self.text_box_4.text = self.row['comp_pf_number']
+    self.text_box_5.text = self.row['comp_esi_number']
+    self.text_box_6.text = self.row['comp_pto_circle']
+  
+  
+  #self.drop_down_1.items = anvil.server.call('comp_change_name_and_code')
 
   def drop_down_1_change(self, **event_args):
     """This method is called when an item is selected"""
-    self.text_box_7.visible = True
-    if self.drop_down_1.selected_value is not None:
-      split_list_comp = self.drop_down_1.selected_value.split("|")
-      split_list_comp = [ele.strip() for ele in split_list_comp] 
-      self.comp_code,self.comp_name = split_list_comp[0],split_list_comp[1]
+    # self.text_box_7.visible = True
+    # if self.drop_down_1.selected_value is not None:
+    #   split_list_comp = self.drop_down_1.selected_value.split("|")
+    #   split_list_comp = [ele.strip() for ele in split_list_comp] 
+    #   self.comp_code,self.comp_name = split_list_comp[0],split_list_comp[1]
   
-      self.row = anvil.server.call('comp_get_details',self.comp_code)
+      # self.row = anvil.server.call('comp_get_details',self.comp_code)
   
-      self.text_box_1.text = self.row['comp_addr1']
-      self.text_box_2.text = self.row['comp_addr2']
-      self.text_box_3.text = self.row['comp_addr3']
-      self.text_box_4.text = self.row['comp_pf_number']
-      self.text_box_5.text = self.row['comp_esi_number']
-      self.text_box_6.text = self.row['comp_pto_circle']
-      self.text_box_7.text = self.comp_name
-      self.drop_down_1.visible = False
-    else:
-      self.button_1.enabled = False
+      # self.text_box_1.text = self.row['comp_addr1']
+      # self.text_box_2.text = self.row['comp_addr2']
+      # self.text_box_3.text = self.row['comp_addr3']
+      # self.text_box_4.text = self.row['comp_pf_number']
+      # self.text_box_5.text = self.row['comp_esi_number']
+      # self.text_box_6.text = self.row['comp_pto_circle']
+    #   self.text_box_7.text = self.comp_name
+    #   self.drop_down_1.visible = False
+    # else:
+    #   self.button_1.enabled = False
       # self.text_box_5.text = self.bank_name
 
   def button_1_click(self, **event_args):
@@ -45,31 +55,11 @@ class comp_change(comp_changeTemplate):
       self.text_box_1.text, self.text_box_2.text,
       self.text_box_3.text,self.text_box_4.text,
       self.text_box_5.text,self.text_box_6.text,
-      self.text_box_7.text)
-    
-    self.clear_inputs()
-    self.drop_down_1.visible=True
-    # refresh after button submit
-    self.drop_down_1.items = anvil.server.call('comp_change_name_and_code')
-
-  def clear_inputs(self):
-    self.text_box_1.text = None
-    self.text_box_2.text = None
-    self.text_box_3.text = None
-    self.text_box_4.text = None
-    self.text_box_5.text = None
-    self.text_box_6.text = None
-    self.text_box_7.text = None
-
-
-  def text_box_7_lost_focus(self, **event_args):
-    """This method is called when the TextBox loses focus"""
-    self.drop_down_1.visible = True
-    self.text_box_7.visible = False
+    #self.drop_down_1.items = anvil.server.call('comp_change_name_and_code')
 
   def button_2_click(self, **event_args):
     """This method is called when the button is clicked"""
-    open_form('comp_change')
+      open_form('comp_change')
 
   def outlined_button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
