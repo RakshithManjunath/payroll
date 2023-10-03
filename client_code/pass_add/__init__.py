42b03b057de862b8a410e8b73812f790445a23bd
systemcomputer.bangalore@gmail.com <systemcomputer.bangalore@gmail.com>
2023-10-03 11:53:45 +0000

    Edited pass_add and settings

--- client_code/pass_add/__init__.py
+++ client_code/pass_add/__init__.py
@@ -12,3 +12,8 @@ class pass_add(pass_addTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def outlined_button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('menu')
+
