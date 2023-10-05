e703293e5a4a06a024ad0f39e17f523c14da9fa7
systemcomputer.bangalore@gmail.com <systemcomputer.bangalore@gmail.com>
2023-10-05 13:47:24 +0000

    Edited comp_change

--- client_code/comp_change/__init__.py
+++ client_code/comp_change/__init__.py
@@ -36,18 +36,20 @@ class comp_change(comp_changeTemplate):
       self.text_box_3.text,self.text_box_4.text,
       self.text_box_5.text,self.text_box_6.text,
       Notification("Company name cannot be blank").show()
-    else:
-      anvil.server.call('comp_update',self.comp_code)
-      self.text_box_1.text, self.text_box_2.text
-      self.text_box_3.text,self.text_box_4.text
-      self.text_box_5.text,self.text_box_6.text
+    # else:
+    #   anvil.server.call('comp_update',self.comp_code)
+    #   self.text_box_1.text, self.text_box_2.text
+    #   self.text_box_3.text,self.text_box_4.text
+    #   self.text_box_5.text,self.text_box_6.tex
+    pass
 
     # # def button_2_click(self, **event_args):
     # #   """This method is called when the button is clicked"""
     # #     open_form('comp_change')
+                        
   def button_2_click(self, **event_args):
     """This method is called when the button is clicked"""
-    open_form('comp_change')
+      open_form('comp_change')
 
     # # def outlined_button_1_click(self, **event_args):
     # #   """This method is called when the button is clicked"""
