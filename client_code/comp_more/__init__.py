d4f8f9c3fec8f336848ed33afa2d7b1060a2859a
systemcomputer.bangalore@gmail.com <systemcomputer.bangalore@gmail.com>
2023-10-13 00:20:58 +0000

    Edited comp_earnhead1, comp_earnhead, comp_misc and comp_more

--- client_code/comp_more/__init__.py
+++ client_code/comp_more/__init__.py
@@ -20,6 +20,8 @@ class comp_more(comp_moreTemplate):
     self.custom_1.visible = not self.custom_1.visible
     self.custom_2.visible = False
     self.custom_3.visible = False
+    self.button_1.enabled = True
+    self.button_2.enabled = True
 
     self.custom_1.refresh()
 
@@ -28,6 +30,8 @@ class comp_more(comp_moreTemplate):
     self.custom_1.visible = False
     self.custom_2.visible = not self.custom_2.visible
     self.custom_3.visible = False
+    self.button_1.enabled = True
+    self.button_2.enabled = True
 
     self.custom_2.refresh()
 
@@ -40,8 +44,72 @@ class comp_more(comp_moreTemplate):
     self.custom_1.visible = False
     self.custom_2.visible = False
     self.custom_3.visible = not self.custom_3.visible
+    self.button_1.enabled = True
+    self.button_2.enabled = True
 
     self.custom_3.refresh()
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.server.call('comp_more_update_earnhead', gvarb.g_comcode,
+                      self.custom_1.text_box_1.text,
+                      self.custom_1.text_box_3.text,
+                      self.custom_1.text_box_5.text,
+                      self.custom_1.text_box_7.text,
+                      self.custom_1.text_box_9.text,
+                      self.custom_1.text_box_11.text,
+                      self.custom_1.text_box_13.text,
+                      self.custom_1.text_box_15.text,
+                      self.custom_1.text_box_17.text,
+                      self.custom_1.text_box_19.text)
+
+    anvil.server.call('comp_earnhead1',gvarb.g_comcode,self.custom_2.check_box_1.checked,
+                      self.custom_2.check_box_2.checked,self.custom_2.check_box_3.checked,
+                      self.custom_2.check_box_4.checked,self.custom_2.check_box_5.checked,
+                      self.custom_2.check_box_6.checked,self.custom_2.check_box_7.checked,
+                      self.custom_2.check_box_8.checked,self.custom_2.check_box_9.checked,
+                      self.custom_2.check_box_10.checked,self.custom_2.check_box_11.checked,
+                      self.custom_2.check_box_12.checked,self.custom_2.check_box_13.checked,
+                      self.custom_2.check_box_14.checked,self.custom_2.check_box_15.checked,
+                      self.custom_2.check_box_16.checked,self.custom_2.check_box_17.checked,
+                      self.custom_2.check_box_18.checked,self.custom_2.check_box_19.checked,
+                      self.custom_2.check_box_20.checked,self.custom_2.check_box_21.checked,
+                      self.custom_2.check_box_22.checked,self.custom_2.check_box_23.checked,
+                      self.custom_2.check_box_24.checked,self.custom_2.check_box_25.checked,
+                      self.custom_2.check_box_26.checked,self.custom_2.check_box_27.checked,
+                      self.custom_2.check_box_28.checked,self.custom_2.check_box_29.checked,
+                      self.custom_2.check_box_30.checked,self.custom_2.check_box_31.checked,
+                      self.custom_2.check_box_32.checked,self.custom_2.check_box_33.checked,
+                      self.custom_2.check_box_34.checked,self.custom_2.check_box_35.checked,
+                      self.custom_2.check_box_36.checked,self.custom_2.check_box_37.checked,
+                      self.custom_2.check_box_38.checked,self.custom_2.check_box_39.checked,
+                      self.custom_2.check_box_40.checked,self.custom_2.check_box_41.checked,
+                      self.custom_2.check_box_42.checked,self.custom_2.check_box_43.checked,
+                      self.custom_2.check_box_44.checked,self.custom_2.check_box_45.checked,
+                      self.custom_2.check_box_46.checked,self.custom_2.check_box_47.checked,
+                      self.custom_2.check_box_48.checked,self.custom_2.check_box_49.checked,
+                      self.custom_2.check_box_50.checked,self.custom_2.check_box_51.checked,
+                      self.custom_2.check_box_52.checked,self.custom_2.check_box_53.checked,
+                      self.custom_2.check_box_54.checked,self.custom_2.check_box_55.checked,
+                      self.custom_2.check_box_56.checked,self.custom_2.check_box_57.checked,
+                      self.custom_2.check_box_58.checked,self.custom_2.check_box_59.checked,
+                      self.custom_2.check_box_60.checked)
+
+    anvil.server.call('comp_misc_leave_loan_update', gvarb.g_comcode, 
+                      self.custom_3.text_box_1.text,
+                      self.custom_3.text_box_2.text, 
+                      self.custom_3.text_box_3.text, 
+                      self.custom_3.text_box_4.text,
+                      self.custom_3.text_box_5.text,
+                      self.custom_3.date_picker_1.date)
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('comp_more')
+
+
+
+
 
 
 
