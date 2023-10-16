d23b48b6678d72da74c20f5692663350d90718d6
systemcomputer.bangalore@gmail.com <systemcomputer.bangalore@gmail.com>
2023-10-16 23:50:35 +0000

    Edited bank_add, bank_change, comp_add, comp_change and 8 more

--- client_code/logform/__init__.py
+++ client_code/logform/__init__.py
@@ -17,20 +17,20 @@ class logform(logformTemplate):
     """This method is called when the button is clicked"""
     data = anvil.server.call('get_all_password')
     if len(list(data)) == 0:
-      if(self.text_box_1.text== 'user1') and (self.text_box_2.text == '1'):
-        self.label_1.visible = False
-        self.label_2.visible = False
-        self.label_4.visible = False        
-        self.text_box_1.visible = False
-        self.text_box_2.visible = False
-        self.button_1.visible = False  
-        self.button_2.visible = False        
-        self.button_3.visible = True
-        self.button_4.visible = True    
-        # self.button_5.visible = True        
+        if(self.text_box_1.text== 'user1') and (self.text_box_2.text == '1'):
+          self.label_1.visible = False
+          self.label_2.visible = False
+          self.label_4.visible = False        
+          self.text_box_1.visible = False
+          self.text_box_2.visible = False
+          self.button_1.visible = False  
+          self.button_2.visible = False        
+          self.button_3.visible = True
+          self.button_4.visible = True    
+          # self.button_5.visible = True        
     else :
-       value = anvil.server.call('check_username_and_password', self.text_box_1.text, self.text_box_2.text)
-       if value == True:
+        value = anvil.server.call('check_username_and_password', self.text_box_1.text, self.text_box_2.text)
+        if value == True:
           open_form('company_select')
 
   def button_3_click(self, **event_args):
@@ -63,18 +63,4 @@ class logform(logformTemplate):
   def button_5_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form('comp_new')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
